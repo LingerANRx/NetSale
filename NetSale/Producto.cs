d268ede69792959ed98cdbd5ef1241574b899c6d
LionCarter <lc2kirito@gmail.com>
2020-06-11 11:40:00 -0500

    [IMP] *Se habilito el Form menu (Version Beta) *Se habilito el boton "Ver pedido" *Se progreso en el Form "Confirmar pedido"

---
+++ NetSale/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSale
+{
+    class Producto
+    {
+        public string producto;
+        public int cantidad;
+        public double precio;
+        public double total;
+        public Producto()
+        {
+
+        }
+    }
+}
